6a2f1369a32261519f8dcf8480a1dd232d3641b7
Anthony Giordano <anthonygiordano1993@gmail.com>
2023-04-25 21:02:10 +0200

    Commit

---
+++ src/main/java/co/develhope/loginDemo/salary/entities/Salary.java
@@ -0,0 +1,23 @@
+package co.develhope.loginDemo.salary.entities;
+
+import co.develhope.loginDemo.user.entities.User;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.Optional;
+
+@Entity
+@Table
+@Data
+public class Salary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private long amount;
+
+    @OneToOne
+    private User user;
+
+    public void setUser(Optional<User> optionalUser) {
+    }
+}
